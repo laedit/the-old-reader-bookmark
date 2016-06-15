0d4181990ad4fdabeb709a4d444888488d7de986
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-06-15 07:27:23 +0200

    Fix run ff dev

--- web-extrunffdev.bat
+++ web-extrunffdev.bat
@@ -6,4 +6,4 @@ echo Exit Code is %errorlevel%
 if "%ERRORLEVEL%" == "1" exit /B 1
 
 :: run web-ext
-web-ext -s "src - Web-Ext" run -firefox-binary "C:\Program Files\Firefox Developer Edition\firefox.exe"
+web-ext -s "src" run --firefox-binary "C:\Program Files\Firefox Developer Edition\firefox.exe"
