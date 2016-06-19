64d0d22aa3b239b48aa05611727902bb5c1b6e3e
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-06-19 17:47:53 +0200

    cleaning

--- src/background-script.js
+++ src/background-script.js
@@ -2,12 +2,11 @@
 
 var currentTabUrl;
 var currentTabIndex;
-var currentTabId;
 
 function handleClick(tab) {
     currentTabUrl = tab.url;
     currentTabIndex = tab.index;
-    currentTabId = tab.id;
+
     if(/^http(?:s?)\:\/\/theoldreader\.com/i.test(tab.url))
     {
         browser.tabs.executeScript({ code: "alert('" + chrome.i18n.getMessage("theOldReaderSelfBookmarkMessage") + "');" });
@@ -22,7 +21,6 @@ function postToOldReadBookmarks(selections) {
     // custom page can't have script injected and can't submit form from their script
     browser.tabs.create({ index: currentTabIndex + 1, url: "https://theoldreader.com/bookmarks/bookmark" }, function (tab) {
         // https://bugzilla.mozilla.org/show_bug.cgi?id=1272890
-        //tab.url = "http:laedit.net";
         browser.tabs.executeScript(tab.id, { file: "postNewBookmark.js" }, function () {
             chrome.tabs.sendMessage(tab.id, {url: currentTabUrl, html: selections});
         });
