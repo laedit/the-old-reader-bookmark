9014f5cf73bac930918f77fb62f802d24a44e9ff
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-27 14:00:40 +0200

    Pass second arg to web-ext - allow --verbose use for example

--- web-extrunff.cmd
+++ web-extrunff.cmd
@@ -7,13 +7,13 @@ if "%ERRORLEVEL%" == "1" exit /B 1
 
 :: run web-ext
 if [%1]==[] (
-    web-ext -s "src" run --firefox-binary "C:\Program Files\Firefox Developer Edition\firefox.exe"
+    web-ext -s "src" run --firefox-binary "C:\Program Files\Firefox Developer Edition\firefox.exe" %2
 )
 
 if [%1]==[current] (
-    web-ext -s "src" run --firefox-binary "C:\Program Files\Mozilla Firefox\firefox.exe"
+    web-ext -s "src" run --firefox-binary "C:\Program Files\Mozilla Firefox\firefox.exe" %2
 )
 
 if [%1]==[beta] (
-    web-ext -s "src" run --firefox-binary "C:\Program Files (x86)\Mozilla Firefox Beta\firefox.exe"
+    web-ext -s "src" run --firefox-binary "C:\Program Files (x86)\Mozilla Firefox Beta\firefox.exe" %2
 )
