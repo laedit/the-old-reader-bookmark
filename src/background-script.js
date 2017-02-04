dc9f059e13f0a423e1de3fe200a829365b304929
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2017-02-04 08:24:51 +0100

    post to blank tab

--- src/background-script.js
+++ src/background-script.js
@@ -1,32 +1,18 @@
 ﻿chrome.browserAction.onClicked.addListener(handleClick);
 
-var currentTabUrl;
-var currentTabIndex;
-
 function handleClick(tab) {
-    currentTabUrl = tab.url;
-    currentTabIndex = tab.index;
-
-    if(/^http(?:s?)\:\/\/theoldreader\.com/i.test(tab.url))
-    {
+    if (/^http(?:s?)\:\/\/theoldreader\.com/i.test(tab.url)) {
         browser.tabs.executeScript({ code: "alert('" + chrome.i18n.getMessage("theOldReaderSelfBookmarkMessage") + "');" });
         return false;
     }
 
-    browser.tabs.executeScript({ file: "content_scripts/getSelection.js" }, postToOldReadBookmarks);
-}
-
-function postToOldReadBookmarks(selections) {
-    // data url not allowed
-    // custom page can't have script injected and can't submit form from their script
-    browser.tabs.create({ index: currentTabIndex + 1, url: "https://theoldreader.com/bookmarks/bookmark" }, function (tab) {
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=1272890
-        browser.tabs.executeScript(tab.id, { file: "content_scripts/postNewBookmark.js" }, function () {
-            chrome.tabs.sendMessage(tab.id, {url: currentTabUrl, html: selections});
+    browser.tabs.executeScript({ file: "content_scripts/getSelection.js" })
+        .then(selections => {
+            browser.tabs.create({ index: tab.index + 1, url: "about:blank" })
+                .then(newTab => {
+                    browser.tabs.executeScript(newTab.id, { matchAboutBlank: true, file: "content_scripts/postNewBookmark.js" }).then(result => {
+                        browser.tabs.sendMessage(newTab.id, { url: tab.url, html: selections });
+                    });
+                });
         });
-    });
 }
-
-chrome.runtime.onMessage.addListener(function (message) {
-    console.log(message.message);
-});
--- src/content_scripts/postNewBookmark.js
+++ src/content_scripts/postNewBookmark.js
@@ -1,18 +1,18 @@
 function submitBookmark(request, sender, sendResponse)
 {
     var f = document.createElement('form');
-    f.setAttribute('method','post');
-    f.setAttribute('action','//theoldreader.com/bookmarks/bookmark');
+    f.setAttribute('method', 'post');
+    f.setAttribute('action', 'https://theoldreader.com/bookmarks/bookmark');
 
     var f1 = document.createElement('input');
-    f1.setAttribute('type','hidden');
-    f1.setAttribute('name','saved_post[url]');
+    f1.setAttribute('type', 'hidden');
+    f1.setAttribute('name', 'saved_post[url]');
     f1.setAttribute('value', request.url);
     f.appendChild(f1);
 
     var f2 = document.createElement('input');
-    f2.setAttribute('type','hidden');
-    f2.setAttribute('name','saved_post[content]');
+    f2.setAttribute('type', 'hidden');
+    f2.setAttribute('name', 'saved_post[content]');
     f2.setAttribute('value', request.html);
     f.appendChild(f2);
 
